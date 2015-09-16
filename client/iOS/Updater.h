c836f61749474325b75baddedddc992b7ea4bc84
Ali Najafizadeh <a.najafizadeh@gmail.com>
2015-09-16 12:40:30 -0400

    added some helper core updater

--- client/iOS/Updater.h
+++ client/iOS/Updater.h
@@ -1,7 +1,12 @@
+@class RCTRootView;
+
 @interface Updater : NSObject
 
-- (id) init;
-- (void) launchUpdateView;
+- (id) initWithModuleName:(NSString *)moduleName;
+- (void) launchUpdaterApp;
 - (void) launchMainApp;
+
+- (void) beforeUpdaterLaunch:(RCTRootView *)launchRootView;
+- (void) beforeMainAppLaunch:(RCTRootView *)launchRootView;
 
 @end
--- client/iOS/Updater.m
+++ client/iOS/Updater.m
@@ -7,20 +7,23 @@
 
 @implementation Updater {
   UINavigationController *_navigator;
+  NSString *_moduleName;
 }
 
-- (id) init {
+- (id) initWithModuleName:(NSString *)moduleName {
   self = [super init];
 
   if (self) {
     _navigator = [[UINavigationController alloc] init];
     [_navigator setNavigationBarHidden:YES animated:NO];
+
+    _moduleName = moduleName;
   }
 
   return self;
 }
 
-- (void) launchUpdater {
+- (void) launchUpdaterApp {
   if ([_navigator.viewControllers count] == 0) {
     NSURL *bundleURL = [[NSBundle mainBundle] URLForResource:@"updater" withExtension:@"jsbundle"];
     UIViewController *updaterViewController = [self rootViewWithModuleName:@"Updater"
@@ -32,29 +35,62 @@
 }
 
 - (void) launchMainApp {
-
+  if ([_navigator.viewControllers count] == 1) {
+    NSURL *bundleURL = [self savedMainAppPathAsURL];
+    UIViewController *updaterViewController = [self rootViewWithModuleName:_moduleName
+                                                                 bundleURL:bundleURL];
+    [_navigator pushViewController:updaterViewController animated:NO];
+  } else {
+    NSLog(@"Error: either upadter is not launched or main app is already launched.");
+  }
 }
 
 - (void) beforeUpdaterLaunch:(RCTRootView *)launchRootView {
 
 }
 
-- (void) beforeMainAppLaucnh:(RCTRootView *)launchRootView {
+- (void) beforeMainAppLaunch:(RCTRootView *)launchRootView {
 
 }
 
 -(UIViewController *) rootViewWithModuleName:(NSString *)moduleName
-                                   bundleURL:(NSURL *)bundleURL{
+                                   bundleURL:(NSURL *)bundleURL {
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:bundleURL
                                                       moduleName:moduleName
                                                initialProperties:nil
                                                    launchOptions:nil];
+  if ([moduleName isEqualToString:@"updater"]) {
+    [self beforeUpdaterLaunch: rootView];
+  } else {
+    [self beforeMainAppLaunch: rootView];
+  }
 
   UIViewController *viewController = [[UIViewController alloc] init];
   viewController.view = rootView;
 
   return viewController;
+}
+
+- (void) saveUpdateBundleWithContent:(NSString *)content {
+  NSURL *urlPath = [self savedMainAppPathAsURL];
+
+  [content writeToFile:[urlPath absoluteString]
+            atomically:YES
+              encoding:NSUTF8StringEncoding
+                 error:nil];
+}
+
+- (NSURL *) savedMainAppPathAsURL {
+  NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,  NSUserDomainMask, YES);
+  NSString *documentsDirectory = [paths objectAtIndex:0];
+  NSString *appFile = [documentsDirectory stringByAppendingPathComponent:@"main.jsbundle"];
+
+  appFile = [NSString stringWithFormat:@"file://%@", appFile];
+
+  NSURL* bundleURL = [NSURL URLWithString:appFile];
+
+  return bundleURL;
 }
 
 @end
