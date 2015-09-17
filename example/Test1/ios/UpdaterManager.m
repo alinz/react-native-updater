9551c08c94cf01a9b3328ca36559c48309daa45e
Ali Najafizadeh <a.najafizadeh@gmail.com>
2015-09-17 13:16:00 -0400

    removed files

--- example/Test1/ios/UpdaterManager.m
+++
@@ -1,9 +0,0 @@
-//
-//  UpdaterManager.m
-//  Test1
-//
-//  Created by Ali Najafizadeh on 2015-09-16.
-//  Copyright (c) 2015 Facebook. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
