a82a14565154836310d158f07d6f76728661409f
Ali Najafizadeh <a.najafizadeh@gmail.com>
2015-09-21 00:41:52 -0400

    added some more code to support router

---
+++ server/api/keys.go
@@ -0,0 +1,16 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/zenazn/goji/web"
+)
+
+func hello(c web.C, w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %s!", c.URLParams["name"])
+}
+
+func init() {
+
+}
