1e5158066c3d96a03ef0f2c37f6e39474fbdf2e8
Ali Najafizadeh <a.najafizadeh@gmail.com>
2015-09-24 22:29:59 -0400

    added db script

---
+++ server/scripts/db.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+database="react_native_updater_db"
+user="react_native_updater"
+password="98uhi4q3brjfnsdlzisw2"
+
+# create postgres USER as SUPERUSER
+psql -h127.0.0.1 <<< "CREATE USER postgres SUPERUSER;"
+
+# removing tracing
+psql -h127.0.0.1 -U postgres <<< "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '$database' AND pid <> pg_backend_pid();"
+
+# dropping database
+psql -h127.0.0.1 -U postgres <<< "DROP DATABASE $database;"
+
+# create database react_native_updater_db
+psql -h127.0.0.1 -U postgres <<< "CREATE DATABASE $database ENCODING 'UTF-8' LC_COLLATE='en_US.UTF-8' LC_CTYPE='en_US.UTF-8' TEMPLATE template0;"
+
+# create username and password
+psql -h127.0.0.1 -U postgres <<< "CREATE USER $user WITH PASSWORD '$password';"
